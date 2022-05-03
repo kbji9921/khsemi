9657e50cd9925af9558ed1d0b01a2439deed0bf2
kbji9921 <kbji9921@naver.com>
2022-05-03 20:05:57 +0900

    충돌 test

--- semi/src/main/java/semi/servlet/DtoDao/PlayerDao.java
+++ semi/src/main/java/semi/servlet/DtoDao/PlayerDao.java
@@ -2,6 +2,7 @@ package semi.servlet.DtoDao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class PlayerDao {
 	public void insert(PlayerDto playerDto) throws Exception{
@@ -22,5 +23,32 @@ public class PlayerDao {
 		ps.execute();
 		
 		con.close();
+	}
+	
+	public PlayerDto selectOne(String playerId)throws Exception{
+		Connection con = JdbcUtils.getConnection();
+		
+		String sql = "select * from player where player_id = ?";
+		PreparedStatement ps = con.prepareStatement(sql);
+		
+		ps.setString(1, playerId);
+		
+		ResultSet rs = ps.executeQuery();
+		PlayerDto playerDto = null;
+		if(rs.next()) {
+			playerDto = new PlayerDto();
+			playerDto.setPlayerId(rs.getString("playerId"));
+			playerDto.setPlayerName(rs.getString("playerName"));
+			playerDto.setPlayerBirth(rs.getDate("playerBirth"));
+			playerDto.setPlayerPhone(rs.getInt("playerPhone"));
+			playerDto.setPlayerEmail(rs.getString("playerEmail"));
+			playerDto.setPlayerPoint(rs.getInt("playerPoint"));
+			playerDto.setPlayerGrade(rs.getString("playerGrade"));
+			playerDto.setPlayerJoin(rs.getDate("playerJoin"));
+			playerDto.setPlayerLogin(rs.getDate("playerLogin"));
+			
+		}
+		con.close();
+		return playerDto;
 	}
 }
