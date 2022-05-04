16332cc8805a70be61fdbc80f7334db75bd4013e
kbji9921 <kbji9921@naver.com>
2022-05-04 18:18:09 +0900

    login, logout, findId, findPw

---
+++ semi/src/main/java/semi/servlet/filter/LoginPlayerFilter.java
@@ -0,0 +1,42 @@
+package semi.servlet.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebFilter(filterName="f2", urlPatterns = {
+		"/player/loginSuccess.jsp"
+})
+public class LoginPlayerFilter implements Filter{
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse resp = (HttpServletResponse) response;
+		
+		try {
+			
+			String playerId = (String) req.getSession().getAttribute("login");
+			
+			if(playerId == null) {
+				resp.sendRedirect("login.jsp");
+			}else {
+				chain.doFilter(req, resp);
+			}
+			
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+			resp.sendError(500);
+		}
+	}
+
+}
--- semi/src/main/webapp/player/login.jsp
+++ semi/src/main/webapp/player/login.jsp
@@ -7,28 +7,33 @@
 <title>로그인</title>
 </head>
 <body>
-	<form action = "login.player" method="post">
+	<form action="login.player" method="post">
+		<label>
+			아이디
+			<input type="text" name="playerId" required placeholder="8~21">
+		</label>
+		
+		<br><br>
+		
+		<label>
+			아이디
+			<input type="password" name="playerPw" required >
+		</label>
+		
+		<br><br>
+		
+		<input type="submit" value="로그인">
 	
-	<label>
-		아이디
-		<input type="text" name="playerId" required placeholder="8~21">
-	</label>
-	<br><br>
+	</form>
 	
-	<label>
-		비밀번호
-		<input type="password" name="playerPw" required>
-	</label>
-	<input type="submit" value="로그인">
-	<br><br>
 	<label>
 		<a href="findIdPlayer.jsp">아이디 찾기</a>
 	</label>
+	
 	<br><br>
+	
 	<label>
 		<a href="findPwPlayer.jsp">비밀번호 찾기</a>
 	</label>
-	
-	</form>
 </body>
 </html>
